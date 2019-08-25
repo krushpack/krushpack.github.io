a79f43adcd859575661519fc9633a47004524676
Krushpack <thekrushpack@gmail.com>
2019-08-25 18:21:04 +0200

    Initial commit

---
+++ olaubranka/menu/menu.js
@@ -0,0 +1,10 @@
+var menuButton = document.querySelector(".menuButton");
+var menu = document.querySelector("#menu");
+
+function menuButtonPressed()
+{
+    if(menuButton.classList.toggle("active"))
+        menu.style.width = "20%";
+    else
+        menu.style.width = "";
+}
